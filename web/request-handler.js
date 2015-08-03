67ec2e48a37d1a595aebd4b24d7c525f3e043350
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-08-03 15:23:14 -0700

    pass first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelper = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+	//console.log(req.url);
+	//console.log(process.cwd());
+
+	if(req.method === "GET"){
+		res.writeHead(200, httpHelper.headers);
+		fs.readFile(process.cwd()+'/web/public/index.html', function(error, content){
+			//console.log(content);
+			res.write(content);
+			res.end();
+		});
+	}
+
+	if(req.method === "POST"){
+
+	}
+
+  //res.end(archive.paths.list);
 };
